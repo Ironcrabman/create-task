2f18241f43e0874822704914d54918a80a8df36b
Andrew Gomez <ootbyc@gmail.com>
2019-03-11 13:23:23 -0400

    work

--- create-task/create.js
+++ create-task/create.js
@@ -44,7 +44,7 @@ let image = document.getElementById('pic')
        description.innerHTML = 'An alligator is attacking your boat!'
        buttonA.innerHTML = 'You wack it in the head with your staff'
        buttonB.innerHTML = 'You try and use a calm spell on it'
-       buttonB.onclick = check = 'magic'
+       check = 'magic'
      } else {
        i = 4
        ////insert first floor
@@ -53,15 +53,35 @@ let image = document.getElementById('pic')
 
    if(i==3){
      if (attack > 5){
-       description.innerHTML = 'The alligator leaves you boat alone and sinks back into the water'
+       description.innerHTML = 'The alligator leaves your boat alone and sinks back into the water'
        buttonA.innerHTML = 'You tie up the boat and head inside'
-       buttonB.innerHTML = 'B: You let the boat float away and head inside the tower'
+       buttonB.innerHTML = 'You let the boat float away and head inside the tower'
      } else {
        description.innerHTML = 'The alligator bites at your staff causing you to fall over but then leaves you alone'
        buttonA.innerHTML = 'You tie up the boat and head inside'
        buttonB.innerHTML = 'You let the boat float away and head inside the tower'
      }
    }
+
+   if(i == 4) {
+       image.src = "images/skeleton.png"
+       description.innerHTML = 'As you make it into the tower, the doors creak as you push them open causing a long dead warrior to awaken'
+       buttonA.innerHTML = 'You cast a beam of white light at it'
+       buttonB.innerHTML = 'You look around the room'
+     }
+
+     if(i == 5) {
+        buttonA.onclick = combat = Math.random()
+        if(combat >= 0.5){
+          description.innerHTML = 'The beam hits the skelton and his causes his bones disintigrate'//Call of Duty Hit Marker Noise Here
+          buttonA.innerHTML = ''
+          buttonB.innerHTML = ''
+        }else{
+          description.innerHTML = 'The skelton rushes at you as you shoot the beam causing '
+          buttonA.innerHTML = ''
+          buttonB.innerHTML = ''
+        }
+      }
 
   }else if (which == 2){
     if (i == 0){
@@ -87,14 +107,13 @@ let image = document.getElementById('pic')
 
     if (i == 2) {
       eventgen();
-      special
       console.log(special)
       if(special == true){
         image.src = "images/mageattack.png"
         description.innerHTML = 'An alligator is approching you!'
         buttonA.innerHTML = 'You wack it in the head with your staff'
         buttonB.innerHTML = 'You try and sneak by it'
-        buttonB.onclick = check = 'stealth'
+        check = "stealth"
         console.log(check)
       } else{
         i=4
@@ -102,7 +121,7 @@ let image = document.getElementById('pic')
     }
 
     if (i == 3) {
-      if (check == 'magic'){
+    if (check == 'magic'){
         description.innerHTML = 'The alligator relaxs its jaw and slides back into the water allowing you to get to the tower'
         buttonA.innerHTML = 'You tie up the boat before heading inside'
         buttonB.innerHTML = 'You go straight into the tower after swimming through the river'
@@ -112,38 +131,32 @@ let image = document.getElementById('pic')
       buttonA.innerHTML = 'You dry off your clothes before heading inside'
       buttonB.innerHTML = 'You go straight into the tower after clearing the river'
     }else {
-      prompt("The alligator lunges from the water grabbing you and pulling you under the water")
+      image.src = "images/darksouls.jpg"
+      description.innerHTML = "You got gamed by the alligator"
+      buttonA.innerHTML = "Restart Your Journey"
+      buttonB.innerHTML = "Quit the Game"
+      buttonA.onclick = location.reload();
+      buttonB.onclick = window.close() ///////fix
     }
-
     }
-
-  }
-
+}
+////Insert here
   if(i == 4) {
-    image.src = "images/skelton.png"
-    description.innerHTML = 'The tower doors creak as you push them open causing a long dead warrior to awaken'
+    image.src = "images/skeleton.png"
+    description.innerHTML = 'As you make it into the tower, the doors creak as you push them open causing a long dead warrior to awaken'
     buttonA.innerHTML = 'You cast a beam of white light at it'
-    buttonA.onclick = combat = Math.random()
-      if(combat >= 0.5){
-        description.innerHTML = 'The beam hits the skelton and his causes his bones disintigrate'//Call of Duty Hit Marker Noise Here
-        buttonA.innerHTML = ''
-        buttonB.innerHTML = ''
-      }else{
-        description.innerHTML = 'The skelton rushes at you as you shoot the beam causing '
-        buttonA.innerHTML = ''
-        buttonB.innerHTML = ''
-      }
     buttonB.innerHTML = 'You look around the room'
   }
 
-}
 
+
+  }
 i++
 }
 
 function eventgen() {
   special = Math.floor((Math.random() * 10) + 1);
-  if(special >= 7){
+  if(special >= 0){ ////////////CHANGE THISS BVACKCKCKCCKCKCKCKCKCK
     special = true
     return special
   }else {
